9c13945d9d99d8a24d92ae8ac3ec3d5360090da6
Luca Manolache <luca1230@gmail.com>
2024-08-06 14:46:28 -0700

    Time: 20 ms (89.13%), Space: 13 MB (6.52%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int comp(const void *a,const void *b) {
+    int *x = (int *) a;
+    int *y = (int *) b;
+    
+    return *y - *x;
+}
+
+int minimumPushes(char* word) {
+    int counts[26] = {};
+    while (*word != NULL) counts[*(word++) - 97]++;
+    
+    qsort(counts, sizeof(counts)/sizeof(*counts), sizeof(*counts), comp);
+    
+    int sum = 0;
+    for (int i = 8; i < 26 + 8; ++i) sum += counts[i - 8] * (i / 8);
+    
+    return sum;
+}

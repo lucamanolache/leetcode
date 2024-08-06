56d635777b7933f4542d9ed93f008f1002f27f8d
Luca Manolache <luca1230@gmail.com>
2024-08-06 14:54:12 -0700

    Time: 26 ms (50.00%), Space: 13 MB (6.52%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.c
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.c
@@ -12,8 +12,7 @@ int minimumPushes(char* word) {
     qsort(counts, sizeof(counts)/sizeof(*counts), sizeof(*counts), comp);
     
     int sum = 0;
-    int* cts = counts;
-    for (int i = 8; i < 26 + 8 && *cts != 0; i++) sum += *(cts++) * (i / 8);
+    for (int i = 8; i < 26 + 8 && counts[i - 8] != 0; i++) sum += counts[i - 8] * (i / 8);
     
     return sum;
 }
